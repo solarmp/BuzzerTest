3bbfc72b4d1e37f84fc86e6c761b60f999f93d9e
solarmp <solaryaz@aol.com>
2014-06-11 00:55:15 +0000

    Adding mongoose

---
+++ models/User.js
@@ -0,0 +1,3 @@
+var mongoose = require('mongoose');
+var User = mongoose.model('User', {name: String, email: String});
+module.exports = User;
--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
+var mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/BuzzerTest')
+var User = require('../models/User');
 /* GET home page. */
 router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
+});
+
+/* Post register.*/
+router.post('/register', function(req, res) {
+  var user = new User({ name: req.body.name,
+                       email: req.body.email});
+  user.save(function(err){
+    if(err) {
+      
+      consol.log('Error inserting the user',err);
+      next();
+      
+    }
+    
+    res.render('members',{ title: 'Members Home'});
+    
+  });
 });
 
 module.exports = router;
